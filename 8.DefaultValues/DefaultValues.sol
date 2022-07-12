ce36f7921a7e69734ea7cab287535eec1e3c9288
"Yashashav DK" <"yashashav.12345@gmail.com">
2022-07-12 23:41:05 +0530

    added lessons

---
+++ 8.DefaultValues/DefaultValues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract DefaultValues {
+    bool public b; // false
+    uint public ui; // 0
+    int public i; // 0
+    address public addr; // 0x00000...40 zeroes
+    bytes32 public b32; // 0x0000... 64 zeroes
+    
+}
